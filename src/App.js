a5afb57705a8c0730fad5f89169227859c46fbfa
everybodyneedsis <jacobkylefranklin@gmail.com>
2021-05-21 13:05:46 -0400

    built out most of the forms, added some styles and photos

--- src/App.js
+++ src/App.js
@@ -1,53 +1,17 @@
-import React, { useState, useEffect } from "react";
-import homePage from './home'
-import pizzaForm from './pizzaForm'
-import Nav from './nav'
-import './App.css'
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
-import * as yup from 'yup'
-import formSchema from './formSchema'
+import * as yup from 'yup';
+import React, {useState} from 'react'
 import axios from 'axios'
-import Help from './Help'
+import PizzaHeader from "./Header";
 
 const App = () => {
   return (
     <>
-    
-      <div>
-        <h1>Lambda Eats</h1>
-      </div>
-
-    <Router>
-      <div>
-        <nav>
-          <ul>
-              <Link to='./homePage'>Home</Link>
-              <Link to='./Help'>Help</Link>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path='./homePage'>
-            <Home />
-          </Route>
-          <Route path='./Help'>
-            <Help />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-
+      {/* <h1>Lambda Eats</h1>
+      <p>You can remove this code and create your own header</p> */}
+      <PizzaHeader />
     </>
   );
 };
-
-function Home() {
-  return <h2>Home</h2>;
-}
-// function Help() {
-//   return <h2>Help</h2>;
-// }
-
 export default App;
 
 
